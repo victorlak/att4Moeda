8f0f4b8f62ad653cdd5646c1775bae50122068b2
victor <133174073+victorlak@users.noreply.github.com>
2024-08-16 21:10:47 -0300

    Add files via upload

---
+++ ConversorReal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+import Interfaces.IConversorMoeda;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ConversorReal implements IConversorMoeda{
+    @Override
+    public void converterParaReal(double dolar) {
+        System.out.println("US$" +dolar+ " em Reais é: R$" +  dolar * 5.49);
+    }
+
+    @Override
+    public void converterParaDolar(double real) {
+        System.out.println("R$"+real+ " em Dolares é: US$" + real / 5.49);
+    }
+}
