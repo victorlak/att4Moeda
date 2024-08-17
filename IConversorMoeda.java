6acb49d9a86d4312d8b6922622e3f12c2e6902df
victor <133174073+victorlak@users.noreply.github.com>
2024-08-16 21:11:22 -0300

    Add files via upload

---
+++ IConversorMoeda.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Interfaces;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface IConversorMoeda {
+    void converterParaReal(double dolar);
+    void converterParaDolar(double real);
+}
